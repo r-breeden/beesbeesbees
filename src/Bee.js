9462dd548d870efbadd19652330b74876cf15f40
Hack Reactor Student <student@hackreactor.com>
2017-07-10 15:41:52 -0700

    BeesBeesBees passes basic requirements

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+//set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+//set the contructor
+Bee.prototype.constructor = Bee; 
 
